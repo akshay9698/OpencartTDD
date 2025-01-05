f285ba2d070fed5dd22d48bc2cc3e04934f89dc7
Akshay Kudale <akshaykudale3@gmail.com>
2025-01-05 18:12:35 +0530

    added search page

---
+++ src/main/java/com/qa/opencart/pages/SearchPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SearchPage {
+	
+	private WebDriver driver;
+	
+	public SearchPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
